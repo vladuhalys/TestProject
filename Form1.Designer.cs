ae0f191036a746cb058c81a5a4265f6489453135
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-09-30 14:32:32 +0300

    task

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,33 +30,47 @@
         {
             button1 = new Button();
             button2 = new Button();
+            button3 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(466, 153);
+            button1.Location = new Point(533, 204);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(173, 77);
+            button1.Size = new Size(198, 103);
             button1.TabIndex = 0;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            button2.Location = new Point(124, 153);
+            button2.Location = new Point(142, 204);
+            button2.Margin = new Padding(3, 4, 3, 4);
             button2.Name = "button2";
-            button2.Size = new Size(173, 77);
+            button2.Size = new Size(198, 103);
             button2.TabIndex = 1;
             button2.Text = "button2";
             button2.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            button3.Location = new Point(320, 405);
+            button3.Name = "button3";
+            button3.Size = new Size(238, 81);
+            button3.TabIndex = 2;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
+            Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -66,5 +80,6 @@
 
         private Button button1;
         private Button button2;
+        private Button button3;
     }
 }
